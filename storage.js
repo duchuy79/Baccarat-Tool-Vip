ae91636b6d8ea6972b1266a53cb238c850b8a70f
duchuy79 <duchuy208.vn+sv2@gmail.com>
2025-12-25 14:38:20 +0700

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,4 +1,9 @@
-export const history=[];
+export const history = [];
 
-export function add(x){ history.push(x); }
-export function undo(){ history.pop(); }
+export function add(x){
+  history.push(x);
+}
+
+export function undo(){
+  history.pop();
+}
